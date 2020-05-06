39c9eaf3eb2dbe5dd212e242f8746c447cddd22b
Charlotte Goldthwaite <chargold201@gmail.com>
2020-05-06 11:47:26 -0400

    Changed home page title to base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis
+  def full_title(title = "")
+    if title == ""
+      "Ruby on Rails Tutorial Sample App"
+    else
+      "#{title}" + " | " + "Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
