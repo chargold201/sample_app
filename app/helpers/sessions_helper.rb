80abe66791d6fda6c1c59aa4de8152681a56d9c0
Charlotte Goldthwaite <chargold201@gmail.com>
2020-06-06 16:58:37 -0400

    Complete basic signup/login/logout functionality

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,21 @@
+module SessionsHelper
+
+  # Logs in given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns the current user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.clear
+    @current_user = nil
+  end
+end
