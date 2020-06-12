cfe4057cce331ad02e5c34b2cceb912a48cfd17b
Charlotte Goldthwaite <chargold201@gmail.com>
2020-06-12 16:38:35 -0400

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user &. authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user &. authenticate(params[:session][:password])
       reset_session
-      log_in user
-      redirect_to user_url(user)
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to user_url(@user)
     else
       flash.now[:danger] = "Incorrect email/password combination"
       render 'new'
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
